8a715286c7d5bef52e3ece6343ccf97e5e4aef0c
evankozliner <evankozliner@gmail.com>
2018-08-22 08:19:45 -0700

    handles emails and sends to SES

--- backend/notifier/app.py
+++ backend/notifier/app.py
@@ -1,19 +1,67 @@
 import json
+import os
 import boto3
 import requests
+from emailer import conversion_email
+import ast
 
-def lambda_handler(event, context):
-    print "REACHED"
-    print event
-    print 'test 2'
+VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
 
-    ip = requests.get('http://checkip.amazonaws.com/')
+RECAPTCHA_FAILED_RES = {
+        "statusCode": 400,
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": json.dumps({
+            'message': 'Invalid Captcha.'
+        })}
 
-    return {
+BAD_REQ_RES = {
+        "statusCode": 400,
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": json.dumps({
+            'message': 'Invalid Request.'
+        })}
+
+SUCCESS_RES = {
         "statusCode": 200,
         "headers": {"Access-Control-Allow-Origin": "*"},
         "body": json.dumps({
-            'message': 'hello world',
-            'location': ip.text.replace('\n', ''),
-        })
-    }
+            'message': 'hello world'
+        })}
+
+def lambda_handler(event, context):
+    conversion_email(ast.literal_eval(event['body']))
+
+    if is_prod() and not request_validation_success(event):
+        return BAD_REQ_RES
+
+    if is_prod() and not recaptcha_success(event):
+        return RECAPTCHA_FAILED_RES
+
+    return SUCCESS_RES
+
+def request_validation_success(event):
+    if not event or not event['body']:
+        return False
+
+    return True
+
+def is_prod():
+    return os.environ['Stage'] == 'prod'
+
+def recaptcha_success(event):
+    form_data = json.loads(event['body'])
+
+    if not 'g-recaptcha-response' in form_data:
+        return False
+
+    recaptcha_response = form_data['g-recaptcha-response']
+
+    recaptcha_secret = os.environ['RecaptchaSecret']
+    google_response = requests.post(VERIFY_URL, data={
+        'secret': recaptcha_secret,
+        'response': recaptcha_response})
+
+    print "Google Recaptcha response: " + str(google_response.content)
+
+    return json.loads(google_response.content)['success']== True
+
---
+++ backend/notifier/emailer.py
@@ -0,0 +1,91 @@
+import boto3
+from botocore.exceptions import ClientError
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SENDER = "Sender Name <getdatingphotos@gmail.com>"
+
+# Replace recipient@example.com with a "To" address. If your account 
+# is still in the sandbox, this address must be verified.
+RECIPIENT = "getdatingphotos@gmail.com"
+
+# Specify a configuration set. If you do not want to use a configuration
+# set, comment the following variable, and the 
+# ConfigurationSetName=CONFIGURATION_SET argument below.
+#CONFIGURATION_SET = "ConfigSet"
+
+# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+AWS_REGION = "us-west-2"
+
+# The subject line for the email.
+SUBJECT = "Amazon SES Test (SDK for Python)"
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+             "This email was sent with Amazon SES using the "
+             "AWS SDK for Python (Boto)."
+            )
+            
+# The HTML body of the email.
+BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Photo meetup request!</h1>
+  <p>Name: {}</p>
+  <p>Email address: {}</p>
+  <p>Phone number: {}</p>
+  <p>Message: {}</p>
+</body>
+</html>
+            """            
+
+# The character encoding for the email.
+CHARSET = "UTF-8"
+
+# Create a new SES resource and specify a region.
+client = boto3.client('ses',region_name=AWS_REGION)
+
+def conversion_email(form_data):
+    print form_data
+    
+    formatted_body = BODY_HTML.format(form_data['name'],
+            form_data['email'],
+            form_data['phone'],
+            form_data['message'])
+
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': formatted_body,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            #ConfigurationSetName=CONFIGURATION_SET,
+        )
+    except ClientError as e:
+        print("ERROR")
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+
